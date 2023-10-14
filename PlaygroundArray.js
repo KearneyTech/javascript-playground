6df9826a22a1b7911bdb34031d9e6f614f22bd24
Kearney <email@kearneytech.com>
2023-10-14 13:33:49 -0700

    Old changes.

--- PlaygroundArray.js
+++ PlaygroundArray.js
@@ -2,21 +2,25 @@
 exports.__esModule = true;
 var PlaygroundArray = /** @class */ (function () {
     function PlaygroundArray(message) {
+        this.arrayString = message;
+        //this.originalExample();
+    }
+    PlaygroundArray.prototype.originalExample = function () {
         var _this = this;
-        var mapExample = message.map(function (x) {
+        var mapExample = this.arrayString.map(function (x) {
             _this.mapHelp(x);
             return x;
         });
-        var filterExample = message.filter(function (x) {
+        var filterExample = this.arrayString.filter(function (x) {
             if (_this.filterHelp(x))
                 return x;
         });
         var initialValue = 0;
-        var reduceExample = message.reduce(this.reduceHelp);
+        var reduceExample = this.arrayString.reduce(this.reduceHelp);
         console.log(mapExample);
         console.log(filterExample);
         console.log(reduceExample);
-    }
+    };
     PlaygroundArray.prototype.mapHelp = function (param) {
         console.log("greet, " + param);
         return param;
@@ -30,8 +34,36 @@ var PlaygroundArray = /** @class */ (function () {
         console.log(index);
         return previousValue + currentValue;
     };
+    /**
+     * The lowest tempurature over the past week.
+     */
+    PlaygroundArray.prototype.reduceWeatherLowByWeek = function (previousValue, currentValue, index) {
+        return previousValue < currentValue ? previousValue : currentValue;
+    };
     return PlaygroundArray;
 }());
 exports["default"] = PlaygroundArray;
 var paramData = ["one", "two", "kool", "kalm"];
-var sample = new PlaygroundArray(paramData);
+var esampleData = {
+    items: [
+        {
+            name: "",
+            value: ""
+        }
+    ],
+    paramData: ["one", "two", "kool", "kalm"]
+};
+//let sample = new PlaygroundArray(paramData);
+//let outerLimits = paramData.reduce(sample.reduceHelp);
+//console.log(outerLimits);
+//object
+//function
+//array
+//execute
+//result
+function func(paramArray) {
+    var reducer = function (sum, val) { return sum + val; };
+    var initialValue = 0;
+    return paramArray.map(function (item) { return item > 1 ? item : 0; }).reduce(reducer, initialValue);
+}
+console.log(func([1, 2, 3, 4]));
--- PlaygroundArray.ts
+++ PlaygroundArray.ts
@@ -8,21 +8,36 @@
  * // Thread1 fire
  * 
  */
-import axios from "axios"
+type complexData = {
+  items : [
+    {
+      name: "",
+      value: ""
+    }
+  ],
+  paramData : ["one", "two","kool","kalm"]
+}
 
 export default class PlaygroundArray {
     greeting: string;
+    arrayString: Array<string>
    
     constructor(message: Array<string>) {
-      let mapExample = message.map(x => {
+      this.arrayString = message;
+
+      //this.originalExample();
+    }
+
+    originalExample() {
+      let mapExample = this.arrayString.map(x => {
         this.mapHelp(x);
         return x;
       });
-      let filterExample = message.filter(x => {
+      let filterExample = this.arrayString.filter(x => {
         if(this.filterHelp(x)) return x;
       });
       let initialValue = 0;
-      let reduceExample = message.reduce(this.reduceHelp);
+      let reduceExample = this.arrayString.reduce(this.reduceHelp);
 
       console.log(mapExample);
       console.log(filterExample);
@@ -42,8 +57,41 @@ export default class PlaygroundArray {
       console.log(index);
       return previousValue + currentValue;
     }
+    /**
+     * The lowest tempurature over the past week.
+     */
+    
+    reduceWeatherLowByWeek(previousValue: string, currentValue: string, index: number) {
+      return previousValue < currentValue ? previousValue : currentValue;
+    }
   }
 
 const paramData = ["one", "two","kool","kalm"];
+const esampleData : complexData = {
+  items : [
+    {
+      name: "",
+      value: ""
+    }
+  ],
+  paramData : ["one", "two","kool","kalm"]
+}
    
-let sample = new PlaygroundArray(paramData);
+//let sample = new PlaygroundArray(paramData);
+//let outerLimits = paramData.reduce(sample.reduceHelp);
+
+//console.log(outerLimits);
+
+//object
+//function
+//array
+//execute
+//result
+
+function func(paramArray) {
+  const reducer = (sum, val) => sum + val;
+  const initialValue = 0;
+  return paramArray.map(item => item > 1 ? item : 0).reduce(reducer, initialValue);
+}
+
+console.log(func([1,2,3,4]));
