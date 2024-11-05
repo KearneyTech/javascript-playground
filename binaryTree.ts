0f32bf7dc1c39479b64c7f622e387e76d68bbbf6
Kearney <email@kearneytech.com>
2024-11-05 11:15:17 -0800

    Binary tree refactoring.

--- binaryTree.ts
+++ binaryTree.ts
@@ -2,11 +2,8 @@
 var height = -1;
 
 // Structure of a Binary Tree Node
-class StudyNode
-{
-
-    constructor()
-    {
+class StudyNode {
+    constructor() {
         this.data = 0;
         this.left = null;
         this.right = null;
@@ -14,12 +11,11 @@ class StudyNode
     data: number;
     left: any;
     right: any;
-};
+}
 
 // Utility function to create
 // a new Binary Tree Node
-function newNode(item)
-{
+function newNode(item) {
     var temp = new StudyNode();
     temp.data = item;
     temp.left = temp.right = null;
@@ -28,41 +24,49 @@ function newNode(item)
 
 // Function to find the depth of
 // a given node in a Binary Tree
-function findDepth(root, x)
-{
-    
+function findDepth(root, x) {
     // Base case
-    if (root == null)
-        return -1;
+    if (root == null) return -1;
 
     // Initialize distance as -1
     var dist = -1;
 
     // Check if x is current node=
-    if ((root.data == x)|| 
-    
+    if (
+        root.data == x ||
         // Otherwise, check if x is
         // present in the left subtree
-        (dist = findDepth(root.left, x)) >= 0 || 
-        
+        (dist = findDepth(root.left, x)) >= 0 ||
         // Otherwise, check if x is
         // present in the right subtree
-        (dist = findDepth(root.right, x)) >= 0)
-
+        (dist = findDepth(root.right, x)) >= 0
+    )
         // Return depth of the node
         return dist + 1;
-        
+
     return dist;
+}
+
+// Function to find the depth of a
+// Binary Tree using depth-first (DFS) approach.
+function findTreeDepth(root) {
+    // Base case
+    if (root == null) return 0;
+
+    // Initialize distance as 0
+    var dist = 0;
+
+    const leftDepth = findTreeDepth(root.left);
+    const rightDepth = findTreeDepth(root.right);
+
+    return Math.max(leftDepth, rightDepth) + 1;
 }
 
 // Helper function to find the height
 // of a given node in the binary tree
-function findHeightUtil(root, x)
-{
-    
+function findHeightUtil(root, x) {
     // Base Case
-    if (root == null)
-    {
+    if (root == null) {
         return -1;
     }
 
@@ -76,17 +80,14 @@ function findHeightUtil(root, x)
     var ans = Math.max(leftHeight, rightHeight) + 1;
 
     // If current node is the required node
-    if (root.data == x)
-        height = ans;
+    if (root.data == x) height = ans;
 
     return ans;
 }
 
 // Function to find the height of
 // a given node in a Binary Tree
-function findHeight(root, x)
-{
-    
+function findHeight(root, x) {
     // Stores height of the Tree
     findHeightUtil(root, x);
 
@@ -96,6 +97,13 @@ function findHeight(root, x)
 
 // Driver Code
 // Binary Tree Formation
+//       5
+//     /   \
+//   10     15
+//  /  \    / \
+// 20  25  30  35
+//       \
+//        45
 var root = newNode(5);
 root.left = newNode(10);
 root.right = newNode(15);
@@ -107,7 +115,10 @@ root.right.right = newNode(35);
 var k = 25;
 // Function call to find the
 // depth of a given node
-console.log("Depth: " + findDepth(root, k)); // 2
+console.log(`Depth: ${findDepth(root, k)}`); // 2
 // Function call to find the
 // height of a given node
-console.log("Height: " + findHeight(root, k)); // 1
+console.log(`Height: ${findHeight(root, k)}`); // 1
+// Function call to find the
+// depth of the tree
+console.log(`Tree Depth: ${findTreeDepth(root)}`); // 4?
